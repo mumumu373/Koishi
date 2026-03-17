a7d12fe429b17790a0da82741dd33c5626bba108
逸樹 <tanaka.ituki819@gmail.com>
2026-03-18 01:31:32 +0900

    マウスのコードだけ追加

---
+++ ACT/SorceCode/CMouseInput/CMouseInput.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <Windows.h>
+#include "MyMath.h"
+#include "Global.h"
+#include <CGame/CCollisionDetection/CCollisionDetection.h>
+
+
+class CMouseInput
+{
+private:
+	int Size;//ゲーム中に変える可能性のある為コンストではない
+	static constexpr int StertSize=64;//ゲーム開始時のサイズ
+	static constexpr int IMGSize = 40;
+public:
+	
+	~CMouseInput();
+
+	//マウスの入力を記録
+	static void Update() { GetInstance()->update(); };
+	void update();
+	//マウスの左を取得,Pushは押したか離しているか Momentは押し続けているか押した瞬間か（trueは押した瞬間）
+	static bool GetMouseLeft(bool Push,bool Moment) { return GetInstance()->getMouseLeft(Push,Moment); };
+	bool getMouseLeft(bool Push, bool Moment);
+
+	//マウスの右を取得,Pushは押したか離しているか Momentは押し続けているか押した瞬間か（trueは押した瞬間）
+	static bool GetMouseRight(bool Push, bool Moment) { return GetInstance()->getMouseRight(Push, Moment); };
+	bool getMouseRight(bool Push, bool Moment);
+
+	//マウスの真ん中の押込みを取得,Pushは押したか離しているか Momentは押し続けているか押した瞬間か（trueは押した瞬間）
+	static bool GetMouseWheel(bool Push, bool Moment) { return GetInstance()->getMouseWheel(Push, Moment); };//マウスホイール
+	bool getMouseWheel(bool Push, bool Moment);
+
+	//マウス画像描画
+	static void Draw(){ GetInstance()->draw(); }
+	void draw();
+
+	static ObjectInfo GetcollisionMouse() {
+		ObjectInfo i;
+			i.x = GetInstance()->MousePos.x - GetInstance()->Size / 2;
+		i.y = GetInstance()->MousePos.y - GetInstance()->Size / 2;
+		i.xw= GetInstance()->Size;
+		i.yh = GetInstance()->Size;
+		return i;
+	}
+	static int GetcollisionMouseSize() {
+		return GetInstance()->Size;
+	}
+	static void ColorChange() {
+		GetInstance()->MouseColorChange = true;
+	}
+	static CMouseInput* GetInstance()
+	{
+		//唯一のインスタンスを作成する
+		//※staticで作成されたので２回目以降は、下の１行は無視される
+		static CMouseInput* s_Instance=new CMouseInput();	//s_:static
+
+		return s_Instance;
+	};
+	static void InitialSettings(HWND hWnd);
+	static VECTOR2_f GetMousePos();
+
+private:
+	//外部からコンストラクタへのアクセスを禁止する
+	CMouseInput();
+
+	//コピーコンストラクタによるコピーを禁止する
+	//=deleteで関数の定義を削除できる
+	CMouseInput(const CMouseInput& rhs) = delete;
+
+	//代入演算子によるコピーを禁止する
+	//operator(オペレーター);演算子のオーバーロードで演算の中身を拡張できる
+	CMouseInput& operator=(CMouseInput& rhs) = delete;
+
+
+	bool MouseColorChange;
+	 HWND m_hWnd;
+	 bool MouseLeft[2] ;
+	 bool MouseRight[2];
+	 bool MouseWheel[2];
+	 VECTOR2_f MousePos;
+};
+
