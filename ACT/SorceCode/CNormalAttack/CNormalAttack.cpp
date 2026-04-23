40886e1f27e2bfb76b51fd2f1368095c98573298
mumumu373 <shirishidaigo.tpr@gmail.com>
2026-04-23 13:12:41 +0900

    エラー直し

--- ACT/SorceCode/CNormalAttack/CNormalAttack.cpp
+++ ACT/SorceCode/CNormalAttack/CNormalAttack.cpp
@@ -4,17 +4,17 @@
 
 
 CNormalAttack::CNormalAttack()
-	:Colision()
+	:Collision()
 	, IMagePos()
 	, Imagerag()
 	, Antimension()
 	, Attacked(false)
 {
-	for (int i = 0; i < ColisionMAX; i++) {
-		Colision[i].x = 0;
-		Colision[i].xw = AtakuSize;
-		Colision[i].y = 0;
-		Colision[i].yh = AtakuSize;
+	for (int i = 0; i < CollisionMAX; i++) {
+		Collision[i].x = 0;
+		Collision[i].xw = AtakuSize;
+		Collision[i].y = 0;
+		Collision[i].yh = AtakuSize;
 	}
 }
 
@@ -26,11 +26,11 @@ void CNormalAttack::Strat(double rag,VECTOR2_f PlayerCenterPos)
 {
 	Antimension = 0;
 	Attacked = true;
-	IMagePos.x = PlayerCenterPos.x +-cos(rag)* (AtakuSize* ColionMAX- AtakuSize /2);
-	IMagePos.y = (PlayerCenterPos.y +-sin (rag)* (AtakuSize* ColionMAX)- AtakuSize/2);
-	for (int i = 0; i < ColionMAX; i++) {
-		Colion[i].x = PlayerCenterPos.x  +-cos(rag) * AtakuSize*(i+1)- AtakuSize/2;
-		Colion[i].y = PlayerCenterPos.y + -sin(rag) * AtakuSize*(i+1) - AtakuSize / 2;
+	IMagePos.x = PlayerCenterPos.x +-cos(rag)* (AtakuSize* CollisionMAX - AtakuSize /2);
+	IMagePos.y = (PlayerCenterPos.y +-sin (rag)* (AtakuSize* CollisionMAX)- AtakuSize/2);
+	for (int i = 0; i < CollisionMAX; i++) {
+		Collision[i].x = PlayerCenterPos.x  +-cos(rag) * AtakuSize*(i+1)- AtakuSize/2;
+		Collision[i].y = PlayerCenterPos.y + -sin(rag) * AtakuSize*(i+1) - AtakuSize / 2;
 	}
 	Imagerag = rag * 180  / M_PI + 180;
 
@@ -52,21 +52,20 @@ void CNormalAttack::Draw(std::unique_ptr<CCamera>& pCamera)
 {
 	if (Attacked == true) {
 		
-		int i = Time / ColisionMAX;
+		int i = Time / CollisionMAX;
 		int t = Antimension / i;
-		VECTOR2_f Pos = { Colion[0].x + Colion[0].xw / 2 - ((AtakuSize * ColionMAX)/2),Colion[0].y + Colion[0].yh - AtakuSize  };
+		VECTOR2_f Pos = { Collision[0].x + Collision[0].xw / 2 - ((AtakuSize * CollisionMAX)/2),Collision[0].y + Collision[0].yh - AtakuSize  };
 		VECTOR2_f pos= pCamera->CalcToPositionInCamera(&Pos);
 		CImageManager::SelectImg(CImageManager::IMG_Slash)->TransAlBlendRotation3(
 			pos.x,
 			pos.y,
+			AtakuSize * CollisionMAX,
 			AtakuSize,
-			AtakuSize * ColionMAX,
-			AtakuSize ,
 			0,
 			32 * t,
 			imageW,
 			imageH,
-			255, 0, 0, Imagerag-90
+			255, 0, 0,Imagerag
 		);
 	}
 	}
@@ -77,16 +76,16 @@ void CNormalAttack::DrawColion(HDC hdc,HWND hwnd, std::unique_ptr<CCamera>& pCam
 	if (Attacked == true) {
 		PAINTSTRUCT ps;
 		HDC hdc1 = BeginPaint(hwnd, &ps);
-		for (int i = 0; i < ColisionMAX; i++) {
+		for (int i = 0; i < CollisionMAX; i++) {
 			VECTOR2_f m_Position;
-			m_Position.x = Colision[i].x;
-			m_Position.y = Colision[i].y;
+			m_Position.x = Collision[i].x;
+			m_Position.y = Collision[i].y;
 			VECTOR2_f DispPos = pCamera->CalcToPositionInCamera(&m_Position);
 			CImageManager::SelectImg(CImageManager::enImgList::IMG_BackSky)->TransAlBlendRotation3(
 				DispPos.x,
 				DispPos.y,
-				Colision[i].xw,
-				Colision[i].yh,
+				Collision[i].xw,
+				Collision[i].yh,
 				0,
 				0,
 				1,
--- ACT/SorceCode/CNormalAttack/CNormalAttack.h
+++ ACT/SorceCode/CNormalAttack/CNormalAttack.h
@@ -5,7 +5,7 @@
 
 class CNormalAttack
 {
-	static constexpr int ColisionMAX = 2;
+	static constexpr int CollisionMAX = 2;
 	static constexpr int imageW = 64;
 	static constexpr int imageH = 32;
 	static constexpr int Time = 6;//継続時間
@@ -20,16 +20,16 @@ public:
 	void DrawColion(HDC hdc, HWND hwnd, std::unique_ptr<CCamera>& pCamera);
 
 	ObjectInfo GetColion(int No) { 
-		return Colision[No]; 
+		return Collision[No];
 	}
 	int GetMAX() {
-		return ColisionMAX;
+		return CollisionMAX;
 	}
 	int GetAttack() {
 		return Attacked;
 	}
 private:
-	ObjectInfo Colision[ColisionMAX];
+	ObjectInfo Collision[CollisionMAX];
 	VECTOR2_f IMagePos;
 	int Imagerag;
 	int Antimension;
